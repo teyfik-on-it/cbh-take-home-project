71e27a3acc8a000c94ec22de0118886d04d1fc1b
Halil Teyfik Dolmaci <halil.dolmaci@creatopy.com>
2022-07-16 15:49:58 +0300

    added test for dpk

--- dpk.test.js
+++ dpk.test.js
@@ -5,4 +5,29 @@ describe("deterministicPartitionKey", () => {
     const trivialKey = deterministicPartitionKey();
     expect(trivialKey).toBe("0");
   });
+
+  it("should return hashed event object", () => {
+    const output = deterministicPartitionKey({ lorem: "ipsum" });
+
+    expect(output).toMatch(/^[\da-f]{128}$/);
+  });
+
+  it("should hash long partitionKey", () => {
+    const input = {
+      partitionKey:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis delectus illo recusandae cum beatae alias reprehenderit dolore nemo labore, ullam, aliquam voluptatem? Tempore quia, aperiam earum magni at minima consequuntur! Similique doloremque explicabo est dignissimos debitis rerum ab rem.",
+    };
+    const output = deterministicPartitionKey(input);
+
+    expect(output.length).toBeLessThanOrEqual(256);
+  });
+
+  it("should not hash short partitionKey", () => {
+    const input = {
+      partitionKey: "Lorem, ipsum dolor sit amet",
+    };
+    const output = deterministicPartitionKey(input);
+
+    expect(output).toBe(input.partitionKey);
+  });
 });
